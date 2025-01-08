06ab1e0b267e33001f454a511955c9713c868594
rodlbor <rodrigoborchert@gmail.com>
2025-01-08 16:26:12 -0300

    test

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-#test te
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import os
+
 app = Flask(__name__)
 
+def fetch_latest_news():
+    api_key = os.getenv('NEWS_API_KEY')  # Get API key from environment variable
+    url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey=8a11b1002cf6437299befc225e850158"
+    response = requests.get(url)
+    data = response.json()
+    return data['articles'][:5]  # Get top 5 articles
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    news = fetch_latest_news()
+    return render_template("index.html", news=news)
 
 if __name__ == '__main__':
-    # Change 127.0.0.1 to 0.0.0.0 to allow external access
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
