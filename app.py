6dc2c3b5a062fa35139e62dbc75f0879dca6e39e
rodlbor <rodrigoborchert@gmail.com>
2025-01-08 16:04:56 -0300

    testing

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +6,6 @@ def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    # Change 127.0.0.1 to 0.0.0.0 to allow external access
+    app.run(host='0.0.0.0', port=5000)
+
