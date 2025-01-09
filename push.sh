e10b3be42cd3bf9c822d034eae7e23bbe4e2dae0
rodlbor <rodrigoborchert@gmail.com>
2025-01-09 13:00:30 -0300

    testing

--- push.sh
+++ push.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Get the current hour
-current_hour=$(date +"%H")
+# Get the current hour, minute, and second
+current_time=$(date +"%H_%M_%S")
 
-# Create a hidden file with the current hour
-touch ~/.current_hour_$current_hour
+# Create a hidden file with the current time
+touch ~/.current_time_$current_time
+
+# Add, commit, and push changes to git
 git add .
 git commit -m "testing"
 git push
+
