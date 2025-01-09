1fe9a87cde458163bc21ea3cf207c0ba2bc9edf5
rodlbor <rodrigoborchert@gmail.com>
2025-01-09 13:05:06 -0300

    testing

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@
 current_time=$(date +"%H_%M_%S")
 
 # Create a hidden file with the current time
-touch ~/.current_time_$current_time
+touch .current_time_$current_time
 
 # Add, commit, and push changes to git
 git add .
